e07a73b0b5e84697167a29b0b8895156a2dbf3f6
Ensick <fede.censi96@gmail.com>
2022-11-25 15:13:44 +0100

    Crezione Componenti Header/Main/Album

--- src/components/AlbumComp.vue
+++ src/components/AlbumComp.vue
@@ -1,15 +1,34 @@
 <template>
-    <div>
-
+    <div class="ms-card-album">
+        <img src="https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg" alt="img">
+        <h2>New Jersey</h2>
+        <h3>1988</h3>
     </div>
 </template>
 
 <script>
     export default {
-        
+        name: 'AlbumComp'
     }
 </script>
 
 <style lang="scss" scoped>
+
+    .ms-card-album{
+
+        width: calc(100% / 5);
+        padding: 25px 15px;
+        aspect-ratio: 2/1;
+
+        img{
+            width: 100%;
+        }
+            
+        h2,h3{
+
+            color: white;
+            text-align: center;
+        }
+    }
 
 </style>
--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,19 +1,33 @@
 <template>
   <main>
-    
+
+    <div>
+
+      <AlbumComp/>
+
+    </div>
+
   </main>
 </template>
 
 <script>
 import axios from 'axios'
+import AlbumComp from './AlbumComp.vue'
 
 export default {
+
+
   name: 'MainComp',
+
+  components:{
+
+    AlbumComp
+  },
   
   data(){
     return{
 
-      arrayDischi: '',
+      arrayAlbum: '',
     
     }
     
@@ -31,7 +45,7 @@ export default {
 
       axios.get('https://flynn.boolean.careers/exercises/api/array/music').then( (response) =>{
 
-        this.arrayDischi = response.data.response
+        this.arrayAlbum = response.data.response
       
       }) 
 
@@ -44,4 +58,7 @@ export default {
 
 <style scoped lang="scss">
 
+  main{
+    background-color: rgba(30, 45, 59, 1);
+  }
 </style>

a7e40b37f616d031840050a28713cb6271d9b7ad
Ensick <fede.censi96@gmail.com>
2022-11-28 15:51:27 +0100

    Aggiunta select

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,13 +1,24 @@
 <template>
   <main>
 
-    <div class="ms-cont-albums">
+    <div class="ms-cont-70">
+
+      <select name="" id="" v-model="valueOptionSelected">
+        <option @click="selezioneGenere()" :value="elem" v-for="(elem,index) in arrayGeneri" :key="index">{{elem}}</option>
+      </select>
+
+
+      <div class="ms-albums">
 
       <AlbumComp v-for="(elem,index) in arrayAlbum" 
       :key="index" 
       :AlbumData = "elem"/>
 
+      </div>
+
     </div>
+
+    
 
   </main>
 </template>
@@ -29,7 +40,10 @@ export default {
   data(){
     return{
 
-      arrayAlbum: '',
+      sceltaGenere: false,
+      valueOptionSelected: '',
+      arrayAlbum: [],
+      arrayGeneri: [],
     
     }
     
@@ -37,22 +51,31 @@ export default {
 
   mounted(){
 
-    this.getData()
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music').then( (response) =>{
+
+      this.arrayAlbum = response.data.response
+
+      this.arrayAlbum.forEach((singoloAlbum) =>{
+
+        if(!this.arrayGeneri.includes(singoloAlbum.genre)){
+
+          this.arrayGeneri.push(singoloAlbum.genre)
+
+          console.log(this.arrayGeneri);
+          
+        }
+        
+      })
+      
+    }) 
   
   },
 
   methods:{
-    
-    getData(){
 
-      axios.get('https://flynn.boolean.careers/exercises/api/array/music').then( (response) =>{
 
-        this.arrayAlbum = response.data.response
-      
-      }) 
-
-    }
   }
+
 }
 
 
@@ -65,14 +88,18 @@ export default {
     padding-top: 6rem;
     background-color: rgba(30, 45, 59, 1);
     
-    .ms-cont-albums{
+    .ms-cont-70{
+
+      width: 70%;
+      margin: 0 auto;
+    }
+
+    .ms-albums{
 
       display: flex;
       flex-wrap: wrap;
       justify-content: space-between;
       gap: 20px 40px;
-      width: 70%;
-      margin: 0 auto;
     }
   }
 </style>
